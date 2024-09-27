37a209640b5577b8f2955109e6f7984dc52add35
stefanoviel <stefi.viel@gmail.com>
2024-09-27 08:54:19 +0200

    removing old stuff

--- imit_ucb/my_gym/my_gym/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from my_gym.envs.gauss_grid_discrete_actions import DiscreteContinuousGridWorld
-from my_gym.envs.gauss_grid_discrete_actions import DiscreteGaussianGridWorld
--- imit_ucb/utils/tools.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-
-def assets_dir(subfolder=None):
-    if subfolder is None:
-        return path.abspath(path.join(path.dirname(path.abspath(__file__)), '../assets'))
-    else:
-        return path.abspath(path.join(path.dirname(path.abspath(__file__)), '../assets/'+subfolder))
