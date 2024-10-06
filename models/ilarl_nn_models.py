d7aada9b50e14f8650050a79ad3a872708db4bad
stefanoviel <stefi.viel@gmail.com>
2024-10-06 13:03:39 +0200

    everything on gpu

--- models/ilarl_nn_models.py
+++ models/ilarl_nn_models.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import pickle
-
 
 # Neural Network architecture for policy, cost function, and value function estimators
 class TwoLayerNet(nn.Module):
@@ -18,115 +16,106 @@ class TwoLayerNet(nn.Module):
         return x
 
 class ImitationLearning:
-    def __init__(self, state_dim, action_dim, num_of_NNs, learning_rate=1e-3, seed=None):
+    def __init__(self, state_dim, action_dim, num_of_NNs, learning_rate=1e-3, device='cpu', seed=None):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.num_of_NNs = num_of_NNs
+        self.device = device
 
-         # Set random seed for reproducibility
         if seed is not None:
             np.random.seed(seed)
             torch.manual_seed(seed)
+            if device == 'cuda':
+                torch.cuda.manual_seed(seed)
         
-        # Initialize neural networks
-        self.policy = TwoLayerNet(state_dim, action_dim)
-        self.reward = TwoLayerNet(state_dim + action_dim, 1)
-        self.z_networks = [TwoLayerNet(state_dim + action_dim, 1) for _ in range(num_of_NNs)]
+        self.policy = TwoLayerNet(state_dim, action_dim).to(device)
+        self.cost = TwoLayerNet(state_dim + action_dim, 1).to(device)
+        self.z_networks = [TwoLayerNet(state_dim + action_dim, 1).to(device) for _ in range(num_of_NNs)]
         
-        # Initialize optimizers
         self.policy_optimizer = optim.Adam(self.policy.parameters(), lr=learning_rate)
-        self.reward_optimizer = optim.Adam(self.reward.parameters(), lr=learning_rate)
+        self.cost_optimizer = optim.Adam(self.cost.parameters(), lr=learning_rate)
         self.z_optimizers = [optim.Adam(net.parameters(), lr=learning_rate) for net in self.z_networks]
         
     def select_action(self, state):
-        state = torch.FloatTensor(state)
-        logits = self.policy(state)
-        action_probs = torch.softmax(logits, dim=-1)
-        action = torch.multinomial(action_probs, 1).item()
+        with torch.no_grad():
+            logits = self.policy(state)
+            action_probs = torch.softmax(logits, dim=-1)
+            action = torch.multinomial(action_probs, 1).item()
         return action
 
     def update_cost(self, expert_states, expert_actions, policy_states, policy_actions, eta):
-        expert_sa = self.encode_actions_concatenate_states(expert_states, expert_actions)
-        policy_sa = self.encode_actions_concatenate_states(policy_states, policy_actions)
+        expert_actions_one_hot = torch.nn.functional.one_hot(torch.tensor(expert_actions, device=self.device), num_classes=self.action_dim)
+        policy_actions_one_hot = torch.nn.functional.one_hot(torch.tensor(policy_actions, device=self.device), num_classes=self.action_dim)
         
-        expert_reward = self.reward(expert_sa).mean()
-        policy_reward = self.reward(policy_sa).mean()
+        expert_sa = torch.vstack([torch.cat((torch.tensor(a1, device=self.device), a2.float())) for a1, a2 in zip(expert_states, expert_actions_one_hot)])
+        policy_sa = torch.vstack([torch.cat((torch.tensor(a1, device=self.device), a2.float())) for a1, a2 in zip(policy_states, policy_actions_one_hot)])
         
+        expert_cost = self.cost(expert_sa).mean()
+        policy_cost = self.cost(policy_sa).mean()
+
         # TODO: check if this is correct
         # we want to increase the expert reward, because we know the expert played well
         # we want to bring the policy reward closer to the expert reward
         # what stops me from increasing the expert reward and decresing the policy reward indefinitely? 
             # the fact that the policy is trained to maximize the reward. So if the reward is high for the expert, the policy will try to get the same reward
             # the policy reward is here just for reference
-        loss = policy_reward - expert_reward # if we use reward. cost: expert_cost - policy_cost
-        
-        self.reward_optimizer.zero_grad()
+        loss = expert_cost - policy_cost
+        self.cost_optimizer.zero_grad()
         loss.backward()
-        self.reward_optimizer.step()
+        self.cost_optimizer.step()
         
         return loss.item()
     
     def update_z_at_index(self, states, actions, rewards, gamma, eta, z_index):
-        sa = self.encode_actions_concatenate_states(states, actions)
+        actions_one_hot = torch.nn.functional.one_hot(torch.tensor(actions, device=self.device), num_classes=self.action_dim)
+        sa = torch.vstack([torch.cat((torch.tensor(a1, device=self.device), a2.float())) for a1, a2 in zip(states, actions_one_hot)])
 
         z_net = self.z_networks[z_index]
         z_opt = self.z_optimizers[z_index]
         
         z_values = z_net(sa).squeeze()
         
-        # Calculate discounted future rewards for each state
         discounted_future_rewards = []
         for i in range(len(rewards)):
             future_rewards = rewards[i:]
             discounted = [gamma**j * r for j, r in enumerate(future_rewards)]
             discounted_future_rewards.append(sum(discounted))
         
-        discounted_future_rewards = torch.FloatTensor(discounted_future_rewards)
+        discounted_future_rewards = torch.FloatTensor(discounted_future_rewards).to(self.device)
         
-        # Compute loss
         loss = torch.mean((z_values - discounted_future_rewards)**2)
         
         z_opt.zero_grad()
         loss.backward()
         z_opt.step()
     
-
     def update_policy(self, states, eta):
-        states = torch.FloatTensor(states)
+        states = torch.FloatTensor(states).to(self.device)
         
-        # Compute Q-values
         states_expanded = states.unsqueeze(1).repeat(1, self.action_dim, 1)  # Shape: [batch_size, action_dim, state_dim]
-        actions = torch.eye(self.action_dim).unsqueeze(0).repeat(states.shape[0], 1, 1)  # Shape: [batch_size, action_dim, action_dim]
+        actions = torch.eye(self.action_dim, device=self.device).unsqueeze(0).repeat(states.shape[0], 1, 1) # Shape: [batch_size, action_dim, action_dim]
         state_action_pairs = torch.cat([states_expanded, actions], dim=2) 
 
-        z_values = torch.stack([z_net(state_action_pairs) for z_net in self.z_networks]) # TODO: check
+        z_values = torch.stack([z_net(state_action_pairs) for z_net in self.z_networks])
 
         z_avg = torch.mean(z_values, dim=0)
         z_std = torch.std(z_values, dim=0)
 
-        rewards = self.reward(state_action_pairs)
+        c_values = self.cost(state_action_pairs)
         
-        # with the reward we want q to be an overestimation of the reward
-        Q = rewards + z_avg + z_std  # Shape: [batch_size, action_dim]
+        Q = c_values + z_avg + z_std # Shape: [batch_size, action_dim]
 
-        # Compute current policy probabilities
         logits = self.policy(states)
         current_probs = torch.softmax(logits, dim=-1)
         
-        # Detach the current probabilities to represent the "old" policy
         old_probs = current_probs.detach()
 
         # Compute the loss
         # we add "-" because we want to maximize the reward and thus q => minimize the loss with a minus in front
         loss = - torch.mean(torch.sum(current_probs * (eta * Q.squeeze(-1) + torch.log(current_probs) - torch.log(old_probs)), dim=1))
 
-        # Update the policy
         self.policy_optimizer.zero_grad()
         loss.backward()
         self.policy_optimizer.step()
 
         return loss.item()
-    
-    def encode_actions_concatenate_states(self, states, actions):
-        actions_one_hot = torch.nn.functional.one_hot(torch.tensor(actions), num_classes=self.action_dim)
-        return torch.vstack([torch.cat((torch.tensor(a1), a2.float())) for a1, a2 in zip(states, actions_one_hot)])
--- train_learner/ilarl_nn.py
+++ train_learner/ilarl_nn.py
@@ -7,13 +7,10 @@ import sys
 import pickle
 import time
 import matplotlib.pyplot as plt
-import gym
-import my_gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import pickle
 from models.ilarl_nn_models import TwoLayerNet, ImitationLearning
 
 def parse_arguments():
@@ -30,11 +27,6 @@ def parse_arguments():
     parser.add_argument('--seed', type=int, default=1, metavar='N')
     parser.add_argument('--eta', type=float, default=1e-3, metavar='G')
     parser.add_argument('--gamma', type=float, default=0.99, metavar='G')
-
-    # parser.add_argument('--beta', type=float, default=100.0, metavar='G',
-    #                     help='log std for the policy (default: -0.0)')
-    # parser.add_argument('--n-expert-trajs', type=int, default=2, metavar='N')
-
     return parser.parse_args()
 
 def create_environment(args):
@@ -47,11 +39,12 @@ def load_expert_trajectories(file_path):
         data = pickle.load(file)
     return data['states'], data['actions']
 
-def collect_trajectory(env, agent, max_steps=10000):
+def collect_trajectory(env, agent, device, max_steps=10000):
     states, actions, rewards = [], [], []
     state = env.reset()
     for iterations in range(max_steps):
-        action = agent.select_action(state)
+        state_tensor = torch.FloatTensor(state).to(device)
+        action = agent.select_action(state_tensor)
         next_state, reward, done, _ = env.step(action)
         
         states.append(state)
@@ -67,55 +60,49 @@ def collect_trajectory(env, agent, max_steps=10000):
     return states, actions, rewards
 
 def plot_visited_states(states):    
-    # TODO: add always plot of the expert
-    # TODO: other plots
     states = np.array(states)
     plt.scatter(states[:, 0], states[:, 1])
     plt.show()
 
-def run_imitation_learning(env, expert_file, max_iter_num, num_of_NNs,seed=None, max_steps=10000):
+def run_imitation_learning(env, expert_file, max_iter_num, num_of_NNs, device, seed=None, max_steps=10000):
     expert_states, expert_actions = load_expert_trajectories(expert_file)
     
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
     
-    il_agent = ImitationLearning(state_dim, action_dim, num_of_NNs, seed=seed)
+    il_agent = ImitationLearning(state_dim, action_dim, num_of_NNs, device=device, seed=seed)
     
     for k in range(max_iter_num):        
-        # Sample policy trajectory
-        expert_traj_states = expert_states[-1]  # only keeping second which is the best trajectory
+        expert_traj_states = expert_states[-1]
         expert_traj_actions = expert_actions[-1]
 
-        policy_states, policy_actions, policy_rewards = collect_trajectory(env, il_agent, max_steps)
-        print(f"Iteration {k}: Cost Loss = {cost_loss:.4f}, Policy Loss = {policy_loss:.4f}, average reward = {np.mean(policy_rewards)}")
+        policy_states, policy_actions, policy_rewards = collect_trajectory(env, il_agent, device, max_steps)
         
-        # Update cost function
         cost_loss = il_agent.update_cost(expert_traj_states, expert_traj_actions, policy_states, policy_actions, args.eta)
         
-        # Update z networks
         for z_index in range(num_of_NNs):
             z_states, z_actions, z_rewards = collect_trajectory(env, il_agent, max_steps) # pased on current policy
 
             # state_action = il_agent.encode_actions_concatenate_states(z_states, z_actions)
             # z_rewards = il_agent.reward(state_action).squeeze().detach().numpy()
-
+            
             # TODO: you can't use real rewards!!!
             il_agent.update_z_at_index(z_states, z_actions, z_rewards, args.gamma, args.eta, z_index)
         
-        # Update policy
         policy_loss = il_agent.update_policy(policy_states, args.eta)
         
+        print(f"Iteration {k}: Cost Loss = {cost_loss:.4f}, Policy Loss = {policy_loss:.4f}, average cost = {np.mean(policy_rewards)}")
+
         if k % 10 == 0:
             plot_visited_states(policy_states)
     
     return il_agent
 
-
 if __name__ == "__main__":
     args = parse_arguments()
     env = create_environment(args)
     
-    il_agent = run_imitation_learning(env, args.expert_trajs, args.max_iter_num, args.num_of_NNs, args.seed)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
     
-
-# python -m train_learner.ilarl_nn --env-name DiscreteGaussianGridworld-v0  --expert-trajs assets/envDiscreteGaussianGridworld-v0type1noiseE0.0/expert_trajs/trajs16.pkl --max-iter-num 50  --grid-type 1 --noiseE 0.0 --seed 1       
+    il_agent = run_imitation_learning(env, args.expert_trajs, args.max_iter_num, args.num_of_NNs, device, args.seed)
